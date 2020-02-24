35aeec4dfc07cad5680ae4df2b57d42fe6fdde29
Bende Siewertsen <bende1308@gmail.com>
2020-02-24 23:06:46 +0100

    added composite service

--- api/src/main/java/com/sieben/api/composite/product/ProductCompositeService.java
+++ api/src/main/java/com/sieben/api/composite/product/ProductCompositeService.java
@@ -4,8 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 public interface ProductCompositeService {
+
+    /**
+     * Sample usage: curl $HOST:$PORT/product-composite/1
+     *
+     * @param productId
+     * @return the composite product info, if found, else null
+     */
     @GetMapping(
-            value = "/product-composite/{productId]",
-            produces = "application/json")
+        value    = "/product-composite/{productId}",
+        produces = "application/json")
     ProductAggregate getProduct(@PathVariable int productId);
 }
--- microservices/product-composite-service/src/main/java/com/sieben/microservices/composite/product/ProductCompositeServiceApplication.java
+++ microservices/product-composite-service/src/main/java/com/sieben/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -1,13 +1,23 @@
 package com.sieben.microservices.composite.product;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
+@ComponentScan("com.sieben")
 @SpringBootApplication
 public class ProductCompositeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductCompositeServiceApplication.class, args);
+	}
+
+	@Bean
+	RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 
 }
